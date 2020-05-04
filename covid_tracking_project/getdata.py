5b5d9d90c2fb660cdbf25d3678783111bf681fe6
Aaron Nielsen <apn@apnielsen.com>
2020-05-04 06:42:02 -0400

    updated to reflect changing api

--- covid_tracking_project/getdata.py
+++ covid_tracking_project/getdata.py
@@ -6,17 +6,35 @@ from matplotlib import pylab as plt
 plt.ion()
 import seaborn as sns
 
+import numpy as np
+import theano.tensor as tt
+import pymc3 as pm
+
 # read data from site
-df = pd.read_json('http://covidtracking.com/api/states/daily?state=OH')
+# api change, 
+#df = pd.read_json('http://covidtracking.com/api/states/daily?state=OH')
+df = pd.read_json('https://covidtracking.com/api/v1/states/daily.json') # gets all states
+# dfs = pd.read_json('https://covidtracking.com/api/v1/states/OH/daily.json') # get Ohio
+
+# limited set of counties
+# dfc = pd.read_json('https://covidtracking.com/api/v1/counties.json')
 
 # convert date into date format
 df['date'] = pd.to_datetime(df['date'],format = '%Y%m%d')
 
+dfs = df[ df['state'] == 'OH' ] 
+
 # plot
 plt.figure()
-sns.lineplot( x=df['date'], y = df['totalTestResults'], data = df )
+sns.lineplot( x=dfs['date'], y = dfs['totalTestResults'], data = dfs )
 plt.grid()
 
 plt.figure()
-sns.lineplot( x=df['date'], y = df['positive'], data = df )
+sns.lineplot( x=dfs['date'], y = dfs['positive'], data = dfs )
 plt.grid()
+
+plt.figure()
+sns.lineplot( x=dfs['date'], y = dfs['death'], data = dfs )
+plt.grid()
+
+
